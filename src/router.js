79e3ef42751454088f0786dfbb5aee7ed5223c2e
Ben Corbin <ben@corbin.digital>
2022-04-12 11:31:14 +0100

    Exercise complete

--- src/router.js
+++ src/router.js
@@ -13,12 +13,28 @@ const mockUser = {
     }
 };
 
+const secret = 'secret123'
+
 router.post('/login', (req, res) => {
 
+	if (req.body.username===mockUser.username && req.body.password === mockUser.password) {
+		res.json(jwt.sign({ username: mockUser.username }, secret));
+	}
+	else {
+		res.status(400)
+		res.json({error: 'Invalid username or password'})
+	}
+	
 });
 
 router.get('/profile', (req, res) => {
-
+	try {
+		const token = req.headers['authorization'];
+		jwt.verify(token, secret);
+		res.status(200).json({ profile: mockUser.profile });
+	} catch (err) {
+		res.status(401).json('Unauthorized');
+	}
 });
 
 module.exports = router;
